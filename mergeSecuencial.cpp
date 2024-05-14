a9fdc1f067dadf647b70fa0708e9ac634dd36a8a
Gaviola <facundo.gaviola@gmail.com>
2024-05-14 15:49:35 -0300

    mejoras al codigo secuencial

--- mergeSecuencial.cpp
+++ mergeSecuencial.cpp
@@ -1,37 +1,41 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <chrono>
+#include <iomanip>
 
-using std::vector, std::printf, std::cout, std::endl;
+
+using namespace std;
 
 void mergeSort(vector<int>&);
-vector<int> merge(std::vector<int>&, std::vector<int>&);
+vector<int> merge(vector<int>&, std::vector<int>&);
 void printList(vector<int>&);
+void checkOrder(vector<int>&);
 
 int main() {
-    std:: ifstream file("/home/fgaviola/CLionProjects/Proyecto_PyD/coso.txt");
-    std::string linea;
+    ifstream file("/home/fgaviola/CLionProjects/Proyecto_PyD/randomListGenerator/results/randomList-1000000.txt");
+    string linea;
     vector<int> list;
 
     int num;
     if (file.is_open()) {
         while (getline(file, linea)) {
-            num = std::stoi(linea);
+            num = stoi(linea);
             list.push_back(num);
         }
     } else {
         cout << "No se pudo abrir el archivo" << endl;
     }
-    cout << "Lista original: " << endl;
-    for (int i : list) {
-        cout << i << " ";
-    }
-    cout << endl;
+    file.close();
+
+
+    cout << list.size() << endl;
+    auto start = chrono::high_resolution_clock::now();
     mergeSort(list);
-    cout << "Lista ordenada: " << endl;
-    for (int i : list) {
-        cout << i << " ";
-    }
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double, std::milli> diff = end - start;
+    checkOrder(list);
+    cout << "Tiempo de ejecución: " << diff.count() << " ms\n";
     return 0;
 }
 
@@ -43,12 +47,8 @@ void mergeSort(vector<int>& list) {
         int medio = list.size() / 2;
         std::vector<int> izquierda(list.begin(), list.begin() + medio);
         std::vector<int> derecha(list.begin() + medio, list.end());
-        printList(izquierda);
-        printList(derecha);
         mergeSort(izquierda);
         mergeSort(derecha);
-        printList(izquierda);
-        printList(derecha);
         vector<int> listOrdenada = merge(izquierda, derecha);
         list = listOrdenada;
     }
@@ -83,5 +83,15 @@ void printList(vector<int>& list) {
         cout << i << " ";
     }
     cout << endl;
+}
+
+void checkOrder(vector<int>& list) {
+    for (int i = 0; i < list.size() - 1; i++) {
+        if (list[i] > list[i + 1]) {
+            cout << "La lista no está ordenada en la posicion" << i << endl;
+            return;
+        }
+    }
+    cout << "La lista está ordenada" << endl;
 }
 
