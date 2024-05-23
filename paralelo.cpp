6ea5d8a61634e3c21e2085474166c3d2d8883255
amag1 <amaglione2002@gmail.com>
2024-05-23 15:28:10 -0300

    Add merge eficiente

--- paralelo.cpp
+++ paralelo.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <queue>
 #include "utils.h"
 #include "reader.h"
 
@@ -10,6 +11,7 @@ using namespace std;
 
 void parallelMergeSort(vector<int>&, int, int);
 void sequentialMerge(vector<int>& list, int world_size, int local_num_elements);
+void mergeK(vector<int>& list, int worldSize, int numElements);
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
@@ -21,7 +23,7 @@ int main(int argc, char** argv) {
     vector<int> list;
 
     if (rank == 0) {
-        list = readFile("/home/andres/Documents/LCC/PPD/Proyecto_PyD/randomListGenerator/results/randomList-13.txt");
+        list = readFile("/home/andres/Documents/LCC/PPD/Proyecto_PyD/randomListGenerator/results/randomList-1000000.txt");
         cout << "Cantidad de elementos: " << list.size() << endl;
     }
 
@@ -39,7 +41,6 @@ int main(int argc, char** argv) {
         }
 
         cout << "Tiempo de ejecución: " << diff.count() << " ms\n";
-        printVector(list);
 
     }
 
@@ -65,12 +66,43 @@ void parallelMergeSort(vector<int>& list, int rank, int world_size) {
 
     //Une las listas en el proceso 0
     MPI_Gather(local_list.data(), local_num_elements, MPI_INT, list.data(), local_num_elements, MPI_INT, 0, MPI_COMM_WORLD);
-
     // Se unen la listas parciales en el proceso 0
     if (rank == 0) {
-        sequentialMerge(list, world_size, local_num_elements);
+        mergeK(list, world_size, local_num_elements);
     }
     //printList(final_list);
+}
+
+void mergeK(vector<int>& list, int worldSize, int numElements) {
+    // Array to keep track of the current index in each sublist
+    vector<int> current_index(worldSize, 0);
+
+    // Priority queue (min heap) to store the elements from different sublists
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+
+    // Initialize the min heap with the first element from each sublist
+    for (int i = 0; i < worldSize; ++i) {
+        if (current_index[i] < numElements) {
+            minHeap.push({list[i * numElements + current_index[i]], i});
+            ++current_index[i];
+        }
+    }
+
+    // Merge the sorted lists
+    int index = 0;
+    while (!minHeap.empty()) {
+        auto minElement = minHeap.top();
+        minHeap.pop();
+
+        // Add the minimum element to the merged list
+        list[index++] = minElement.first;
+
+        // If there are more elements in the same sublist, add the next element to the min heap
+        if (current_index[minElement.second] < numElements) {
+            minHeap.push({list[minElement.second * numElements + current_index[minElement.second]], minElement.second});
+            ++current_index[minElement.second];
+        }
+    }
 }
 
 void sequentialMerge(vector<int>& list, int world_size, int local_num_elements) {
